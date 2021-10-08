cb4115332fe11f132e778d8807ec7978dbab325c
FHenric <ffilipehhenrique@gmail.com>
2021-10-08 13:32:36 -0300

    sad

--- src/database/migrations/bunker/1631916766754-CreateTabProduto.ts
+++ src/database/migrations/bunker/1631916766754-CreateTabProduto.ts
@@ -1,54 +1,54 @@
-import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+// import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
 
-export class CreateTabProduto1631916766754 implements MigrationInterface {
+// export class CreateTabProduto1631916766754 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name: "tab_produto",
-                columns: [
-                    {
-                        name: "id",
-                        type: "integer",
-                        isPrimary: true,
-                        isGenerated: true,
-                        generationStrategy: "increment"
-                    },
-                    {
-                        name: "id_categoria",
-                        type: "integer",
-                        length: "5"
-                    },
-                    {
-                        name: "nome",
-                        type: "varchar",
-                        length: "50"
-                    },
-                    {
-                        name: "valor_unit",
-                        type: "numeric",
-                        precision: 8,
-                        scale: 2
-                    },
-                    {
-                        name: "qtd_estoque",
-                        type: "integer",
-                        length: "15"
-                    },                          
-                ]
-            })
-        )
+//     public async up(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.createTable(
+//             new Table({
+//                 name: "tab_produto",
+//                 columns: [
+//                     {
+//                         name: "id",
+//                         type: "integer",
+//                         isPrimary: true,
+//                         isGenerated: true,
+//                         generationStrategy: "increment"
+//                     },
+//                     {
+//                         name: "id_categoria",
+//                         type: "integer",
+//                         length: "5"
+//                     },
+//                     {
+//                         name: "nome",
+//                         type: "varchar",
+//                         length: "50"
+//                     },
+//                     {
+//                         name: "valor_unit",
+//                         type: "numeric",
+//                         precision: 8,
+//                         scale: 2
+//                     },
+//                     {
+//                         name: "qtd_estoque",
+//                         type: "integer",
+//                         length: "15"
+//                     },                          
+//                 ]
+//             })
+//         )
 
-        await queryRunner.createForeignKey("tab_produto", new TableForeignKey({
-            columnNames:["id_categoria"],
-            referencedColumnNames: ["id"],
-            referencedTableName: "tab_categoria",
-            name: "fk_produto_categoria"
-        }))
-    }
+//         await queryRunner.createForeignKey("tab_produto", new TableForeignKey({
+//             columnNames:["id_categoria"],
+//             referencedColumnNames: ["id"],
+//             referencedTableName: "tab_categoria",
+//             name: "fk_produto_categoria"
+//         }))
+//     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("tab_produto")
-    }
+//     public async down(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.dropTable("tab_produto")
+//     }
 
-}
+// }
