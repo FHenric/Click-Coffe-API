b65c7a12ac576b7da6cd87cab6ba04d70b4b0f14
FHenric <ffilipehhenrique@gmail.com>
2021-10-13 18:28:47 -0300

    asdas

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -3,7 +3,7 @@ import multer from 'multer';
 const upload = multer({
     storage: multer.diskStorage({
         destination:(req, file, cb) => {
-            cb(null,  '../uploads')
+            cb(null,  './dist/uploads')
         },
         filename: (req, file, cb) => {
             cb( null, file.originalname)
